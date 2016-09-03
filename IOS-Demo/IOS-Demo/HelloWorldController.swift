39086aa7d4a6cdc8718b0aff8e8f28e0c8c5dc89
sasa cocic <sasacocic@gmail.com>
2016-09-03 16:55:05 -0500

    added home page that will handle authentication

--- IOS-Demo/IOS-Demo/HelloWorldController.swift
+++ IOS-Demo/IOS-Demo/HelloWorldController.swift
@@ -7,8 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
 class HelloWorldController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidAppear(animated: Bool) {
+        if let user = FIRAuth.auth()?.currentUser {
+            print("navigate somewhere else because a user is already signed in")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +31,13 @@ class HelloWorldController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func signIn() {
+        //sign user in....
+        //then redirect them to the main page
+    }
     
+    @IBAction func signUP() {
+        //create the user and put them in the database
+    }
 
 }
