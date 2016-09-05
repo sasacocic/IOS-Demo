307ed80a000b286613ccdad93ed1078d92b9b492
sasa cocic <sasacocic@gmail.com>
2016-09-05 13:33:00 -0500

    update info.plist file & implmented basic sign in and sign up features

--- IOS-Demo/IOS-Demo/HelloWorldController.swift
+++ IOS-Demo/IOS-Demo/HelloWorldController.swift
@@ -17,7 +17,18 @@ class HelloWorldController: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         if let user = FIRAuth.auth()?.currentUser {
-            print("navigate somewhere else because a user is already signed in")
+            print("navigate somewhere else because a user is already signed in \(user.email!)")
+            //sign them out. this is just for testing
+            do {
+                try FIRAuth.auth()?.signOut()
+            } catch let signoutError as NSError {
+                print("Error signing out: \(signoutError)")
+            }
+            return
+        }
+        else{
+            print("NO ONE IS SIGNED IN GOOD!")
+            return
         }
     }
     
@@ -31,13 +42,37 @@ class HelloWorldController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func signIn() {
-        //sign user in....
+    func signIn(user: FIRUser?) {
+        
+        performSegueWithIdentifier("HomePage", sender: nil)
+        
         //then redirect them to the main page
+    }
+    @IBAction func signInTapped(sender: AnyObject) { //sender is never used in this method...
+        let theUsername = username.text!
+        let thePassword = username.text!
+        FIRAuth.auth()!.signInWithEmail(theUsername, password: thePassword) { (user,error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            self.signIn(user) // if we get to here then the user has been signed in
+        }
     }
     
     @IBAction func signUP() {
         //create the user and put them in the database
+        let email = username.text!
+        let password = username.text!
+        FIRAuth.auth()?.createUserWithEmail(email, password: password) { (user,error) in
+            if let err = error {
+                print(err.localizedDescription)
+                return
+            }
+            print(user?.email!)
+            self.signIn(user)
+        }
+        
     }
 
 }
